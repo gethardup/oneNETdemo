de991725e17194a910c2c46b26dea85bb77c69fc
yanghuan <18228044169@163.com>
2017-06-28 17:17:42 +0800

    webstorm

--- oneNETdemo/app.js
+++ oneNETdemo/app.js
@@ -1,3 +1,19 @@
 /**
  * Created by yanghuan on 2017/6/28.
  */
+let express=require("express");
+let bodyParser=require("body-parser");
+let path=require("path");
+let route=require("./routes");
+let app=express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extend:true}));
+app.use(express.static(path.join(__dirname,"public")));
+app.set("views",path.join(__dirname,"views"));
+app.set("view engine","html");
+app.engine(".html",require("ejs").renderFile);
+route(app);
+app.listen("3000",function (err) {
+    if(err){console.log(err)}
+    console.log("server has run on the port of %s",3000)
+});
--- oneNETdemo/routes/index.js
+++ oneNETdemo/routes/index.js
@@ -1,3 +1,8 @@
 /**
  * Created by yanghuan on 2017/6/28.
  */
+module.exports=function (app) {
+    app.get("/",function (req, res) {
+        res.render("index",{title:"OneNET home page"});
+    })
+};
